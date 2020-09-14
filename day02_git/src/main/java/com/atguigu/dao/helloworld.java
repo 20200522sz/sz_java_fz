b61eb4d1de26cfb0412ac1435920c684801e756d
ps <ps@atguigu.com>
2020-09-14 18:21:45 +0800

    //创建了一个hello类

---
+++ day02_git/src/main/java/com/atguigu/dao/helloworld.java
@@ -0,0 +1,17 @@
+package com.atguigu.dao;
+
+/**
+ * Date:2020/9/14
+ * Author:ybc
+ * Description
+ */
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("hello"); System.out.println("hello"); System.out.println("hello"); System.out.println("hello"); System.out.println("hello");
+
+        
+
+
+    }
+}
